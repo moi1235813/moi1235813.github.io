806d2ef33241bbd2b9640e240c6f4ecb44847354
moi1235813 <86200992+moi1235813@users.noreply.github.com>
2021-12-23 00:42:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var character = document.getElementById("character");
-var block = document.getElemntById("block");
+var block = document.getElementById("block");
 function jump(){
   if(character.classList != "animate"){
      character.classList.add("animate");
